3b508aa1bad7aa16e29ef36d475f5ac4ab0af5e4
KHANH NGO <KHANHQ.NGO@LibertyMutual.com>
2018-03-08 00:38:15 -0500

    Built first page with input field.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/reel.png';
 import './App.css';
+import './Style/flatly.min.css';
+import './main';
+import ReactDOM from 'react-dom';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+            <img src={logo} className="App-logo" alt="logo" />
+            <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title" href="app.js">Movie Search</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+          <div>
+
+          <div className="container">
+              <div className="jumbotron">
+                  <h3 className="text-center">Enter Key Word for the Movie you want to search</h3>
+                  <form id="searchForm">
+                      <input type="text" className="form-control" id="searchText" placeholder="Search Movies..." />
+                  </form>
+              </div>
+          </div>
+
+          <div className="container">
+              <div id="movies" className="row"></div>
+          </div>
+              <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+              <script src="main.js"></script>
+          </div>
       </div>
     );
   }
 }
+
+ReactDOM.render(<main />, document.getElementById('root'));
 
 export default App;
---
+++ src/main.js
@@ -0,0 +1,8 @@
+// import React, { Component } from 'react';
+import $ from 'jquery';
+
+$(document).ready(() => {
+    console.log('on Main.js');
+});
+
+
