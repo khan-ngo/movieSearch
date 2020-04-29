064a1852e1ab445ca58602c8c0d6dfb1e4265c1c
khan <khan.ngo@gmail.com>
2020-04-29 15:16:28 -0400

    Updates depenedncies

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,48 +1,46 @@
 "use strict";
 
-import React, { Component } from 'react';
-import axios from 'axios';
-import ReactDOM from 'react-dom';
-
-
+import React, { Component } from "react";
+import axios from "axios";
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = { movies: [] };
+  }
 
-    constructor() {
-        super();
-        this.state = {movies: []};
-    }
+  componentWillMount() {
+    const apiKey = "&apikey=1ddd3fff";
+    const encodedKey = encodeURI(apiKey);
 
-    componentWillMount() {
-        const apiKey = '&apikey=1ddd3fff';
-        const encodedKey = encodeURI(apiKey);
+    axios
+      .get("http://www.omdbapi.com/?s=" + searchString + encodedKey)
+      .then((response) => {
+        this.setState({
+          movies: response,
+        });
+      });
+  }
 
-        axios.get('http://www.omdbapi.com/?s=' + searchString + encodedKey)
-            .then((response) => {
-                this.setState({
-                    movies: response
-                });
-            });
-    };
+  render() {
+    var items = _.map(this.state.movies, (movie) => {
+      if (_.includes(movie.Title, "Star Wars")) {
+        return movie.Title;
+      }
+    });
 
+    items = _.compact(items);
 
-    render(){
-        var items = _.map(this.state.movies, (movie) => {
-            if(_.includes(movie.Title, "Star Wars")){
-                return movie.Title;
-            }
-        })
+    items = _.map(items, (item) => {
+      return <li>{item}</li>;
+    });
 
-        items = _.compact(items);
-
-        items = _.map(items, (item) => {
-            return <li>{item}</li>;
-        })
-
-        return <div><ul>{items}</ul></div>;
-    }
-
+    return (
+      <div>
+        <ul>{items}</ul>
+      </div>
+    );
+  }
 }
-
 
 export default Home;
